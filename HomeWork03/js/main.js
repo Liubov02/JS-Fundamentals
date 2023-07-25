d6b5e560f7db5494219097dbabcc9fcaea816b1b
Liubov02 <lyubovbazhaluk@gmail.com>
2023-07-25 20:17:19 +0300

    made changes

--- HomeWork03/js/main.js
+++ HomeWork03/js/main.js
@@ -171,3 +171,23 @@
 // console.log(calc(7, 4, 2));
 // console.log(calc(7, 4, 3));
 // console.log(calc(7, 4, 4));
+
+
+
+// Task07
+// (Ускладнене. Задача не оцінюється. Для тих, кому хочеться поробити ще щось)
+// Створити функцію create() , яка приймає один аргумент у вигляді рядка. Ця функція повертає анонімну функцію, яка перевіряє чи переданий в неї аргумент збігається з аргументом зовнішньої функції.
+// const tom = create("pass_for_Tom");
+// tom("pass_for_Tom"); //повертає true 
+// tom("pass_for_tom"); //повертає false
+
+function create(secret) {
+  return function(input) {
+    return input === secret;
+  };
+}
+
+const tom = create("pass_for_Tom");
+
+console.log(tom("pass_for_Tom")); // Поверне true
+console.log(tom("pass_for_tom")); // Поверне false
